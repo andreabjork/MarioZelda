ef7a9bf15cae04ec5d3461db9324c074c96ab0c8
Henrý <hthj6@hi.is>
2015-11-11 22:59:16 +0000

    update to make clouds a bit more random

--- src/cloud.js
+++ src/cloud.js
@@ -5,22 +5,22 @@ function Cloud(descr) {
 	
 	this.sprite = this.sprite || g_sprites.cloud;
 	this.cx = Math.random()*2*g_canvas.width - g_canvas.width;
-	this.cy = 0.66*g_canvas.height-0.3*g_canvas.height;
-	this.scale = 0.3+0.3*Math.random();
+	this.cy = g_canvas.height - (0.3 + Math.random())*g_canvas.height;
+	this.scale = 0.4+0.5*Math.random();
 	this.xVel = 0.2+0.3*Math.random();
 }
 
 Cloud.prototype.cx = Math.random()*g_canvas.width;
 Cloud.prototype.scale = 0.2+0.6*Math.random();
-Cloud.prototype.cy = 0.66*g_canvas.height-0.3*g_canvas.height;
+Cloud.prototype.cy = 0.66*g_canvas.height-0.5*g_canvas.height;
 Cloud.prototype.xVel = 0.2+0.3*Math.random();
     
 Cloud.prototype.update = function(du){
 	this.cx += this.xVel * du; 
 	if(this.cx > g_canvas.width + 600){
 		this.cx = -(1+Math.random())*g_canvas.width;
-		this.cy = 0.66*g_canvas.height-0.3*g_canvas.height;
-		this.scale = 0.1+0.2*Math.random();
+		this.cy = g_canvas.height - (0.3 + Math.random())*g_canvas.height;
+		this.scale = 0.4+0.5*Math.random();
 		this.xVel = 0.2 + 0.3*Math.random();
 	}
 };
